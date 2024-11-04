5d8dbe354213d7d0e39d227ac6970b4b94a48041
andre-ls <andre.luis.e3@gmail.com>
2024-11-04 09:25:26 -0300

    Adição de AMI ao EC2

--- Projeto2/modules/ec2/main.tf
+++ Projeto2/modules/ec2/main.tf
@@ -1,7 +1,21 @@
+data "aws_ami" "image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023-*"]
+  }
+}
+
 resource "aws_instance" "ec2_instance" {
   instance_type           = var.instance_type
   subnet_id               = var.subnet_id
   vpc_security_group_ids  = var.security_group_ids
+  ami                     = data.aws_ami.image.id
 
   user_data = <<-EOF
               apt-get update -y
