d07b2ba75e39d3addfa593d4983343349480273e
andre-ls <andre.luis.e3@gmail.com>
2024-11-05 22:00:26 -0300

    Fix Definição de API Gateway

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -42,7 +42,6 @@ resource "aws_api_gateway_method_response" "proxy" {
 resource "aws_api_gateway_integration_response" "proxy" {
   description = "Mapeamento de Resposta do Lambda para API"
   rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = aws_api_gateway_method_response.proxy.status_code
