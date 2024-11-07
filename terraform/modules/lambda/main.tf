56a8eaa6075e3258edb512bc4d5c57aaa3b09725
andre-ls <andre.luis.e3@gmail.com>
2024-11-07 17:25:55 -0300

    Add exisiting role to Lambda

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -8,33 +8,33 @@ resource "aws_lambda_function" "sync_lambda" {
   description = "Recurso da Função Lambda"
   filename = "index.zip"
   function_name = "sync_requests"
-  role = aws_iam_role.lambda_role.arn
+  role = "arn:aws:iam::038160823904:role/LabRole"
   handler = "index.handler"
   runtime = "nodejs14.x"
   source_code_hash = data.archive_file.lambda_package.output_base64sha256
 }
 
 #Role da Função Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+#resource "aws_iam_role" "lambda_role" {
+#  name = "lambda-role"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole",
+#        Effect = "Allow",
+#        Principal = {
+#          Service = "lambda.amazonaws.com"
+#        }
+#      }
+#    ]
+#  })
+#}
 
 #Anexo de Policy a Role da Função Lambda
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role = aws_iam_role.lambda_role.name
+  role = "arn:aws:iam::038160823904:role/LabRole"
 }
 
 #Adição de Permissão de Execução por API Gateway à Função Lambda
