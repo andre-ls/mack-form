b9c08f16f784c4cb81539bfd054f5204e3163730
andre-ls <andre.luis.e3@gmail.com>
2024-11-13 20:21:59 -0300

    Fix Secondary API References

--- terraform/modules/secondary_api_gateway/main.tf
+++ terraform/modules/secondary_api_gateway/main.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "secondary_api" {
-  name = "sync-api"
+  name = "secondary-api"
   description = "API para Consultas à Camada Secundária do Sistema"
 
   endpoint_configuration {
@@ -8,14 +8,14 @@ resource "aws_api_gateway_rest_api" "secondary_api" {
 }
 
 resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  parent_id = aws_api_gateway_rest_api.sync_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.secondary_api.id
+  parent_id = aws_api_gateway_rest_api.secondary_api.root_resource_id
   path_part = var.api_path
 }
 
 #Método de Consulta da API
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+  rest_api_id = aws_api_gateway_rest_api.secondary_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = "POST"
   authorization = "NONE"
@@ -23,7 +23,7 @@ resource "aws_api_gateway_method" "proxy" {
 
 #Definição de Resposta da API
 resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+  rest_api_id = aws_api_gateway_rest_api.secondary_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = "200"
