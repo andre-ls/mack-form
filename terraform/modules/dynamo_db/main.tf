8c9e5982c1aff76e02dba1ec6412fa14f33147ca
andre-ls <andre.luis.e3@gmail.com>
2024-11-13 19:25:30 -0300

    Fix DynamoDb sample item againx2

--- terraform/modules/dynamo_db/main.tf
+++ terraform/modules/dynamo_db/main.tf
@@ -15,10 +15,10 @@ resource "aws_dynamodb_table_item" "user_item" {
   table_name = aws_dynamodb_table.users.id
   hash_key   = "UserId"
   item = <<ITEM
-  {
-    "UserId"   : "user123",
-    "UserName" : "Luke Skywalker",
-    "Email"    : "usetheforceluke@example.com"
-  }
-  ITEM
+{
+"UserId"   : "user123",
+"UserName" : "Luke Skywalker",
+"Email"    : "usetheforceluke@example.com"
+}
+ITEM
 }
