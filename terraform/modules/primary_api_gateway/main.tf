a2a4a3ffa793e9f4d48779cfaba43f616a189440
andre-ls <andre.luis.e3@gmail.com>
2024-11-18 17:56:49 -0300

    Add Submodule Connection to Sync API

--- terraform/modules/primary_api_gateway/main.tf
+++ terraform/modules/primary_api_gateway/main.tf
@@ -1,24 +1,38 @@
-resource "aws_api_gateway_rest_api" "sync_api" {
-  name = "sync-api"
-  description = "API para Consultas Síncronas ao Sistema"
+#resource "aws_api_gateway_rest_api" "sync_api" {
+#  name = "sync-api"
+#  description = "API para Consultas Síncronas ao Sistema"
+#
+#  endpoint_configuration {
+#    types = ["REGIONAL"]
+#  }
+#}
+#
+#resource "aws_api_gateway_resource" "root" {
+#  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+#  parent_id = aws_api_gateway_rest_api.sync_api.root_resource_id
+#  path_part = var.api_path
+#}
+#
+##Método de Consulta da API
+#resource "aws_api_gateway_method" "proxy" {
+#  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = "POST"
+#  authorization = "NONE"
+#}
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
+##Definição de Resposta da API
+#resource "aws_api_gateway_method_response" "proxy" {
+#  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.proxy.http_method
+#  status_code = "200"
+#}
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  parent_id = aws_api_gateway_rest_api.sync_api.root_resource_id
-  path_part = var.api_path
-}
-
-#Método de Consulta da API
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = "POST"
-  authorization = "NONE"
+module "api_gateway" {
+  source = "../submodules/api_gateway"
+  api_name = "sync_api"
+  api_path = var.api_path
 }
 
 #Mapeamento de Consulta da API para o Lambda
@@ -29,14 +43,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type = "AWS"
   uri = var.lambda_arn
-}
-
-#Definição de Resposta da API
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
 }
 
 #Mapeamento de Resposta do Lambda para API
