d737b82a060b84d3f493d9d18e2d0ae9f2ab5eb6
andre-ls <andre.luis.e3@gmail.com>
2024-11-18 18:26:23 -0300

    Add API Gateway Outputs

--- terraform/modules/primary_api_gateway/main.tf
+++ terraform/modules/primary_api_gateway/main.tf
@@ -29,6 +29,29 @@
 #  status_code = "200"
 #}
 
+##Mapeamento de Consulta da API para o Lambda
+#resource "aws_api_gateway_integration" "lambda_integration" {
+#  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.proxy.http_method
+#  integration_http_method = "POST"
+#  type = "AWS"
+#  uri = var.lambda_arn
+#}
+#
+##Mapeamento de Resposta do Lambda para API
+#resource "aws_api_gateway_integration_response" "proxy" {
+#  rest_api_id = aws_api_gateway_rest_api.sync_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.proxy.http_method
+#  status_code = aws_api_gateway_method_response.proxy.status_code
+#
+#  depends_on = [
+#    aws_api_gateway_method.proxy,
+#    aws_api_gateway_integration.lambda_integration
+#  ]
+#}
+
 module "api_gateway" {
   source = "../submodules/api_gateway"
   api_name = "sync_api"
@@ -37,9 +60,9 @@ module "api_gateway" {
 
 #Mapeamento de Consulta da API para o Lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
+  rest_api_id = module.api_gateway.api_id
+  resource_id = module.api_gateway.root
+  http_method = module.api_gateway.http_method
   integration_http_method = "POST"
   type = "AWS"
   uri = var.lambda_arn
@@ -47,13 +70,12 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 #Mapeamento de Resposta do Lambda para API
 resource "aws_api_gateway_integration_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.sync_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
+  rest_api_id = module.api_gateway.api_id 
+  resource_id = module.api_gateway.root 
+  http_method = module.api_gateway.http_method
+  status_code = module.api_gateway.proxy.status_code
 
   depends_on = [
-    aws_api_gateway_method.proxy,
     aws_api_gateway_integration.lambda_integration
   ]
 }
--- terraform/modules/submodules/api_gateway/outputs.tf
+++ terraform/modules/submodules/api_gateway/outputs.tf
@@ -1,3 +1,19 @@
 output "api_arn" {
    value = aws_api_gateway_rest_api.api_gateway.execution_arn
 }
+
+output "api_id" {
+   value = aws_api_gateway_rest_api.api_gateway.id
+}
+
+output "root" {
+   value = aws_api_gateway_resource.root.id
+}
+
+output "http_method" {
+   value = aws_api_gateway_method.proxy.http_method
+}
+
+output "proxy_status_code" {
+   value = aws_api_gateway_method_response.proxy.status_code
+}
