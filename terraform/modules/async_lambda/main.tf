bc73f3a70b33c8e4bb35f4a80f040b1bb1005a1b
andre-ls <andre.luis.e3@gmail.com>
2024-11-19 19:33:49 -0300

    Remove Lambda Async Commentaries

--- terraform/modules/async_lambda/main.tf
+++ terraform/modules/async_lambda/main.tf
@@ -1,23 +1,3 @@
-#data "archive_file" "async_lambda_package" {
-#  type = "zip"
-#  source_file = "${path.module}/index.js"
-#  output_path = "${path.module}/index.zip"
-#}
-#
-#resource "aws_lambda_function" "async_lambda" {
-#  description = "Recurso da Função Lambda para Consultas Assíncronas."
-#  filename = "${path.module}/index.zip"
-#  function_name = "async_requests"
-#  role = "arn:aws:iam::038160823904:role/LabRole"
-#  handler = "index.handler"
-#  runtime = "nodejs20.x"
-#  source_code_hash = data.archive_file.async_lambda_package.output_base64sha256
-#  vpc_config {
-#    subnet_ids         = [var.lambda_subnet]
-#    security_group_ids = [var.lambda_security_group]
-#  }
-#}
-
 module "lambda" {
   source = "../../submodules/lambda"
   function_name = "async_requests"
--- terraform/modules/async_lambda/outputs.tf
+++ terraform/modules/async_lambda/outputs.tf
@@ -1,7 +1,3 @@
-#output "lambda_arn" {
-#   value = aws_lambda_function.async_lambda.arn
-#}
-
 output "lambda_arn" {
    value = module.lambda.lambda_arn
 }
