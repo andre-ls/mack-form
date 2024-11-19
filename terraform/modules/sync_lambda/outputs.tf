578f7b079717fc1a30726c379a1c2a07bee45d7b
andre-ls <andre.luis.e3@gmail.com>
2024-11-19 19:46:21 -0300

    Add Sync Lambda Connection to Submodule

--- terraform/modules/sync_lambda/outputs.tf
+++ terraform/modules/sync_lambda/outputs.tf
@@ -1,3 +1,7 @@
+#output "lambda_arn" {
+#   value = aws_lambda_function.sync_lambda.invoke_arn
+#}
+
 output "lambda_arn" {
-   value = aws_lambda_function.sync_lambda.invoke_arn
+   value = module.lambda.lambda_arn
 }
