14708c7dc947e96ce3b35ac6707a751ac3bdba8a
andre-ls <andre.luis.e3@gmail.com>
2024-11-19 22:51:11 -0300

    Fix Lambda Output

--- terraform/modules/sync_lambda/outputs.tf
+++ terraform/modules/sync_lambda/outputs.tf
@@ -3,5 +3,5 @@
 #}
 
 output "lambda_arn" {
-   value = module.lambda.lambda_arn
+   value = module.lambda.lambda_invoke_arn
 }
--- terraform/submodules/lambda/outputs.tf
+++ terraform/submodules/lambda/outputs.tf
@@ -2,6 +2,10 @@ output "lambda_arn" {
    value = aws_lambda_function.lambda.arn
 }
 
+output "lambda_invoke_arn" {
+   value = aws_lambda_function.lambda.invoke_arn
+}
+
 output "function_name" {
    value = aws_lambda_function.lambda.function_name
 }
