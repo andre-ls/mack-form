2e5ab880bebb9af3529ce0a901dfa81ed41e0fcd
andre-ls <andre.luis.e3@gmail.com>
2024-11-20 01:07:33 -0300

    Remove Sync Lambda Commentary

--- terraform/modules/sync_lambda/main.tf
+++ terraform/modules/sync_lambda/main.tf
@@ -1,23 +1,3 @@
-#data "archive_file" "sync_lambda_package" {
-#  type = "zip"
-#  source_file = "${path.module}/index.js"
-#  output_path = "${path.module}/index.zip"
-#}
-#
-#resource "aws_lambda_function" "sync_lambda" {
-#  description = "Recurso da Função Lambda para funções Síncronas."
-#  filename = "${path.module}/index.zip"
-#  function_name = "sync_requests"
-#  role = "arn:aws:iam::038160823904:role/LabRole"
-#  handler = "index.handler"
-#  runtime = "nodejs20.x"
-#  source_code_hash = data.archive_file.sync_lambda_package.output_base64sha256
-#  vpc_config {
-#    subnet_ids         = [var.lambda_subnet]
-#    security_group_ids = [var.lambda_security_group]
-#  }
-#}
-
 module "lambda" {
   source = "../../submodules/lambda"
   function_name = "sync_requests"
--- terraform/modules/sync_lambda/outputs.tf
+++ terraform/modules/sync_lambda/outputs.tf
@@ -1,7 +1,3 @@
-#output "lambda_arn" {
-#   value = aws_lambda_function.sync_lambda.invoke_arn
-#}
-
 output "lambda_arn" {
    value = module.lambda.lambda_invoke_arn
 }
